4f709568ca61a6c269bdaf710df6d8df7167b4b3
Mohit Gupta <mohit.gupta2jan@gmail.com>
2020-12-22 21:04:27 +0530

    Create PumpkinDB.py

---
+++ PumpkinDB.py
@@ -0,0 +1,23 @@
+import os  # os module is required to manipulate directories and files
+import json  # JSON module helps to manipulate json data
+import platform # Platform information
+import re  # RegExp support for our app
+import shutil  # This module helps to delete databases
+
+banner = """\n\n  \
+██████╗  ██╗   ██╗ ███╗   ███╗ ██████╗  ██╗  ██╗ ██╗ ███╗   ██╗ \n  \
+██╔══██╗ ██║   ██║ ████╗ ████║ ██╔══██╗ ██║ ██╔╝ ██║ ████╗  ██║ \n  \
+██████╔╝ ██║   ██║ ██╔████╔██║ ██████╔╝ █████╔╝  ██║ ██╔██╗ ██║ \n  \
+██╔═══╝  ██║   ██║ ██║╚██╔╝██║ ██╔═══╝  ██╔═██╗  ██║ ██║╚██╗██║ \n  \
+██║      ╚██████╔╝ ██║ ╚═╝ ██║ ██║      ██║  ██╗ ██║ ██║ ╚████║ \n  \
+╚═╝       ╚═════╝  ╚═╝     ╚═╝ ╚═╝      ╚═╝  ╚═╝ ╚═╝ ╚═╝  ╚═══╝ \n  \
+                                                                \n  \
+         ██████╗  ██████╗                                       \n  \
+    ██╗  ██╔══██╗ ██╔══██╗        ██╗                           \n  \
+    ╚═╝  ██║  ██║ ██████╔╝     ██╗╚═╝████████████╗  ██╗         \n  \
+    ██╗  ██║  ██║ ██╔══██╗     ╚═╝██╗╚═══════════╝  ╚═╝         \n  \
+    ╚═╝  ██████╔╝ ██████╔╝        ╚═╝                           \n  \
+         ╚═════╝  ╚═════╝                                       \n\n\
+"""
+
+print(banner)
